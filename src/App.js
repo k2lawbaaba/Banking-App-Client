e99d996895ad61634847a4218a65c0d055c63e66
kolawole abiodun <kolawole.abiodun2016@gmail.com>
2023-10-11 23:00:58 +0100

    update on index.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import Login from "./Components/Login";
 import "./App.css";
 import { ApolloClient, InMemoryCache, ApolloProvider,createHttpLink} from '@apollo/client';
 
-const httpLink = createHttpLink({
-  // uri: "https://banking-api-mcxq.onrender.com/graphql",
-  uri: "http://localhost:4004/graphql",
-});
+// const httpLink = createHttpLink({
+//   // uri: "https://banking-api-mcxq.onrender.com/graphql",
+//   uri: "http://localhost:4004/graphql",
+// });
 
 const client = new ApolloClient({
-  link: httpLink,
+  uri: "https://banking-api-mcxq.onrender.com/graphql",
   cache: new InMemoryCache(),
   credentials:"include",
 
